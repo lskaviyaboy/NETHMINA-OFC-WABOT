14a1b5a5a52e375e9b6f8026661e4d6072cec68f
NethminaOFC <nethminab20@gmail.com>
2024-10-27 19:36:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "xS8mmBjZ#xkX29zoAk_EE49rFUfFWdndYWXgPyJmubvtFksV0xSo",
-MONGODB: process.env.MONGODB || "enter mongodb here",
+SESSION_ID: process.env.SESSION_ID || "BzUl1ZQI#p-v5GZskogzsRnLEa9Q2toXZTW1NiHHFF4GBVPGkFxI",
+MONGODB: process.env.MONGODB || "mongodb+srv://nethminabhashitha2009:p9nUB7v8jqlQpxsu@cluster0.dyxda.mongodb.net/",
 };
